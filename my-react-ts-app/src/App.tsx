c7928017dbda41875a5dc3721e3a88ab41ade768
Daeeun Kang <daeeun123@daum.net>
2024-12-07 00:35:15 +0900

    first commit

---
+++ my-react-ts-app/src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Hello from "./components/Hello";
+
+function App() {
+  return <Hello name="Typescript"></Hello>;
+}
+
+export default App;
---
+++ my-react-ts-app/test.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+const sum: string = add(1, 2); // Error
+console.log(sum);
